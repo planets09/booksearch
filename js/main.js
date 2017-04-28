a640bc2b3dae16628ca2792b76d0a89748730cc0
Rena Tan <rena.tanrx@gmail.com>
2017-04-28 12:55:51 -0700

    reformat background, imagesand links

--- js/main.js
+++ js/main.js
@@ -18,24 +18,19 @@ btn.className = 'btn btn-default btn-md';
 
 // btn.type = "button";
 
-//create text variable and target the ID:
-// var btn = document.getElementById('btn');
-// var results = document.getElementById('results');
 
 //create textnode:
 var h1Text = document.createTextNode('Book Search');
-var inputText = document.createTextNode('search here!');
 var btnText = document.createTextNode('SUBMIT');
 // console.log(h1Text);
-// console.log(inputText);
+// console.log(btnText);
 
-// use .appendChild to append the text node to element:
+//create attribute:
+input.placeholder = 'search here!'
+
+// use .appendChild to append textNode or variable to element:
 h1.appendChild(h1Text);
-input.appendChild(inputText);
 btn.appendChild(btnText);
-// console.log(h1);
-// console.log(input);
-
 div.appendChild(divContainer);
 divContainer.appendChild(h1);
 divContainer.appendChild(input);
@@ -44,13 +39,11 @@ divContainer.appendChild(divResults);
 
 document.getElementById('googleBooks').appendChild(div);
 
-// console.log(div);
 
 //Google books API:
 function bookSearch(){
-	//create text variable to assign the input's value.
+//create text variable to assign the input's value.
 	var search = input.value;
-	console.log(search);
 //Setting results.innerHTML to empty string will empty out the results div, so that
 // whatever is searched next can take up that space.
 	results.innerHTML = "";
@@ -61,18 +54,23 @@ function bookSearch(){
 		dataType: 'json',
 //using GET to bring information back from API.
 		type: 'GET',
-//The data parameter is the entire JSON file retrieved from server. Reminder, the parameter is just a variable.
+//The data parameter is the entire JSON file retrieved from server. Reminder, the 
+//parameter is just a variable.
 		success: function(books){
 			console.log(books);
-//books is the name of large object, one of the property is items array.
+//books is the name of large object, one of the property is 'items' array.
 			for(var i=0; i < books.items.length; i++){
 				var img = document.createElement('img');
 				var a = document.createElement('a');
+//
 				img.src = books.items[i].volumeInfo.imageLinks.thumbnail;
 				a.href = books.items[i].volumeInfo.previewLink;
+
+				img.className = "animated infinite pulse"
 				a.target = "_blank";
 				a.appendChild(img);
-//books is the name of large object, the volumeInfo property in array and there is a title property. This all goes inside the results div:
+//books is the name of large object, the 'volumeInfo' property in array and there is a 
+//title property. This all goes inside the results div:
 				results.innerHTML += '<h2>'+ books.items[i].volumeInfo.title + '</h2>';
 				results.innerHTML += '<h2>'+ books.items[i].volumeInfo.authors + '</h2>';
 				results.innerHTML += '<h3>'+ books.items[i].volumeInfo.publisher + '</h3>';
@@ -82,8 +80,15 @@ function bookSearch(){
 		}
 	});
 }
-
 //These are the parameters for addEventListener: ('type', listener, useCapture)
 // example: btn.addEventListener('type', listener, useCapture);
 btn.addEventListener('click', bookSearch, false);
+
+
+//Example: immediately invoked expression. You targetted the body to run the program.
+// (function() {
+// 	var body = document.getElementById('body')
+// 	body.addEventListener('click', bookSearch, false);
+// })();
+
 
